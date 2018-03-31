f034b72e6f50530a6da93591e3ec45cf3314bc16
Ally Netzel <netzelally@gmail.com>
2018-03-31 03:13:29 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,6 +3,7 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
+    binding.pry
     yield(array[i])
     i += 1
   end
@@ -10,4 +11,4 @@ def hello(array)
 end
 
 binding.pry 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
